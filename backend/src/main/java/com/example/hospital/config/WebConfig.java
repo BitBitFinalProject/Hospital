7011ffe629ed793affc84391aac1f1846114e1ff
Seovin <ksj042302@naver.com>
2025-04-17 13:47:01 +0900

    ãport 3000으로 통일

--- backend/src/main/java/com/example/hospital/config/WebConfig.java
+++ backend/src/main/java/com/example/hospital/config/WebConfig.java
@@ -1,4 +1,15 @@
 package com.example.hospital.config;
 
-public class WebConfig {
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000") // 👈 프론트 포트
+                .allowedMethods("*");
+    }
 }
